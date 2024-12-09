999b9f6aabbfc0628f22fd4f8ebf5c8bbb98afad
Pavithra-tech07 <pavithrakumark2002@gmail.com>
2024-12-09 16:06:37 +0000

    Menu update

--- src/MYPROJECT/Menu.jsx
+++ src/MYPROJECT/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom';
+import {  Outlet } from 'react-router-dom';
  import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Ensure Bootstrap JS is imported
  import img1 from './logo.png';
